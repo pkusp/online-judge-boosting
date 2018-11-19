65fd562f80726acdd73f7a8dc5cf50319d4fd33c
pkusp <pkusp@outlook.com>
2018-11-19 17:52:27 +0800

    <fea> kmp

--- classic_algorithm/kmp.py
+++ classic_algorithm/kmp.py
@@ -22,9 +22,23 @@ def str_match(p,t):
                 return i
     return -1
 
+def get_next(pattern):
+    k=0
+    next = [0]*len(pattern)
+    for i in range(1,len(pattern)):
+        while k>0 and pattern[i] != pattern[k]:
+            k= next[k-1]
+        if pattern[i] == pattern[k]:
+            k+=1
+        next[i] = k
+    return next
+
+
 
 if __name__ == '__main__':
-    pp = 'abc'
-    tt = 'abc'
-    pos = str_match(pp,tt)
-    print(pos)
+    pp = 'abcab'
+    # tt = 'abc'
+    # pos = str_match(pp,tt)
+    # print(pos)
+    next = get_next(pp)
+    print("next:",next)
